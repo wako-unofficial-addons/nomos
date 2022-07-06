87bf8d6304089bad52c0096b5a7a514f2a930d2b
Davy <davidreedmichigan@gmail.com>
2022-07-06 16:06:16 +0200

    upgrade to sdk v7 + Angular 14

--- projects/plugin/src/plugin/plugin.module.ts
+++ projects/plugin/src/plugin/plugin.module.ts
@@ -1,27 +1,30 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 import { IonicModule } from '@ionic/angular';
-import { PluginService } from './services/plugin.service';
 import { MovieButtonComponent } from './movie-button/movie-button.component';
+import { PluginService } from './services/plugin.service';
 
-import { PluginBaseModule } from '@wako-app/mobile-sdk';
-import { SettingsComponent } from './settings/settings.component';
-import { TranslateModule } from '@ngx-translate/core';
-import { EpisodeButtonComponent } from './episode-button/episode-button.component';
 import { FormsModule } from '@angular/forms';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { TranslateModule } from '@ngx-translate/core';
+import { PluginBaseModule, WakoProviders } from '@wako-app/mobile-sdk';
+import { EpisodeButtonComponent } from './episode-button/episode-button.component';
+import { EpisodeItemOptionComponent } from './episode-item-option/episode-item-option.component';
 import { OpenButtonComponent } from './open-button/open-button.component';
 import { KodiPluginService } from './services/kodi-plugin.service';
-import { EpisodeItemOptionComponent } from './episode-item-option/episode-item-option.component';
 import { ToastService } from './services/toast.service';
+import { SettingsComponent } from './settings/settings.component';
 
 const components = [MovieButtonComponent, EpisodeButtonComponent, EpisodeItemOptionComponent, SettingsComponent, OpenButtonComponent];
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule.forRoot(), TranslateModule.forRoot()],
+  imports: [CommonModule, FormsModule, IonicModule.forRoot(),    IonicStorageModule.forRoot({}), TranslateModule.forRoot()],
   declarations: [...components],
   entryComponents: [...components],
-  providers: [PluginService, KodiPluginService, ToastService]
+  providers: [PluginService, KodiPluginService, ToastService,
+    ...WakoProviders,
+  ], // Add your services here. Do not use provideIn: 'root' in your services
 })
 export class PluginModule extends PluginBaseModule {
   static pluginService = PluginService;
